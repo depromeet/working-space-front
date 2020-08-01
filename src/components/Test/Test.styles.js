9e0604b23e325df84afe4e613a3fe0637e6cd8ae
simsimjae <simsimjae@naver.com>
2020-08-01 15:05:08 +0900

    컴포넌트 이름 생성

---
+++ src/components/Test/Test.styles.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const TestStyled= styled.div`
+display: ${props => props.hasButton ? 'block': 'inline-block'};
+    .hello {
+
+    }
+    .asdfa {
+
+    }
+    .asdf asdf{ 
+
+    }
+    .asdf {
+
+    }
+    .asdf {
+
+    }
+
+    .qef {
+
+    }
+
+    .asdf {
+
+    }
+
+    .qwef {
+
+    }
+`;
--- src/containers/SampleContainer.js
+++ src/containers/SampleContainer.js
@@ -16,6 +16,7 @@ const SampleContainer = () => {
 		console.log('컨테이너에서는 UI로직과 관련 없는 라우팅, 도메인 데이터 가공등의 역할을 합니다.')
 		console.log(toJS(SampleStore.sampleList));
 		console.log('이런식으로 Mobx의 Store에 접근해서 데이터를 가져와서 자식에게 내려줍니다.');
+		// 여기서 라우팅
 	}, [SampleStore.sampleList]);
 	
 	return <Sample onClick={onClickButton} userList={toJS(SampleStore.sampleList)}/>;
