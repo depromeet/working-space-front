6e8020e7dc9bf0ae533d9c035c6c099c6d843017
simsimjae <191429@ssg.com>
2020-08-06 17:05:06 +0900

    애니메이션 라이브러리 붙이는중

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,55 +1,60 @@
-import React, { useCallback, useState } from 'react';
-import ModalStyled from './Modal.styles';
+import React, { useCallback, useState } from "react";
+import { spring, Motion } from "react-motion";
+import ModalStyled from "./Modal.styles";
 
 const Modal = props => {
-	const { OpenButton, CloseButton, title } = props;
-	const [isOpen, setIsOpen] = useState(props.isOpen);
-	const onClickOpen = useCallback(() => {
-		setIsOpen(true);
-	}, []);
-	const onClickClose = useCallback(() => {
-		setIsOpen(false);
-	}, []);
+  const { OpenButton, CloseButton, title } = props;
+  const [isOpen, setIsOpen] = useState(props.isOpen);
+  const onClickOpen = useCallback(() => {
+    setIsOpen(true);
+  }, []);
+  const onClickClose = useCallback(() => {
+    setIsOpen(false);
+  }, []);
 
-	return (
-		<>
-			<OpenButton onClick={onClickOpen} />
-			{isOpen && (
-				<ModalStyled>
-					<div className="header">
-						<div className="header_left"></div>
-						<div className="header_center">
-							<span>{title}</span>
-						</div>
-						<div className="header_right">
-							<CloseButton onClick={onClickClose}></CloseButton>
-						</div>
-					</div>
-					<div className="contents">this is contents</div>
-					<div className="footer">
-						<button type="button" className="submit_button">
-							완료
-						</button>
-					</div>
-				</ModalStyled>
-			)}
-		</>
-	);
+  return (
+    <>
+      <OpenButton onClick={onClickOpen} />
+      {isOpen && (
+        <Motion defaultStyle={{ top: 900 }} style={{ top: spring(0, { stiffness: 120, damping: 17 }) }}>
+          {style => (
+            <ModalStyled top={style.top}>
+              <div className="header">
+                <div className="header_left"></div>
+                <div className="header_center">
+                  <span>{title}</span>
+                </div>
+                <div className="header_right">
+                  <CloseButton onClick={onClickClose}></CloseButton>
+                </div>
+              </div>
+              <div className="contents">this is contents</div>
+              <div className="footer">
+                <button type="button" className="submit_button">
+                  완료
+                </button>
+              </div>
+            </ModalStyled>
+          )}
+        </Motion>
+      )}
+    </>
+  );
 };
 
 Modal.defaultProps = {
-	OpenButton: props => (
-		<button type="button" {...props}>
-			열기
-		</button>
-	),
-	CloseButton: props => (
-		<button type="button" {...props}>
-			닫기
-		</button>
-	),
-	title: '카페명',
-	isOpen: false,
+  OpenButton: props => (
+    <button type="button" {...props}>
+      열기
+    </button>
+  ),
+  CloseButton: props => (
+    <button type="button" {...props}>
+      닫기
+    </button>
+  ),
+  title: "카페명",
+  isOpen: false,
 };
 
 export default Modal;
