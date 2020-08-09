7fc7c9201c1239313060b6c7a34c3e88a7bbc90a
leejiwonn <bbongwa@naver.com>
2020-08-10 01:21:13 +0900

    Modal Button css 수정

--- src/containers/DetailContainer.js
+++ src/containers/DetailContainer.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { observer } from "mobx-react";
 import useStore from "../hooks/useStore";
 import Detail from "../components/Detail/Detail";
+import Modal from "../components/Modal/Modal";
 
 const DetailContainer = () => {
   const { CardStore } = useStore();
@@ -10,7 +11,12 @@ const DetailContainer = () => {
     CardStore.fetchCard();
   }, [CardStore]);
 
-  return <Detail />;
+  return (
+    <>
+      <Detail />
+      <Modal />
+    </>
+  );
 };
 
 export default observer(DetailContainer);
