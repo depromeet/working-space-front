883c01db8a3c94d9f5fab596e0dcce707ed34f7d
simsimjae <simsimjae@naver.com>
2020-08-09 15:01:54 +0900

    rating star 수정

--- src/components/RatingStar/RatingStar.styles.js
+++ src/components/RatingStar/RatingStar.styles.js
@@ -1,26 +1,19 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
 const RatingStarStyled = styled.div`
-	display: flex;
-	flex-direction: ${props => (props.simple ? 'row' : 'column')};
-	align-items: center;
-	width: 100%;
-	.rating_area {
-		margin-top: -2px;
-	}
-	.react-stars {
-		> span {
-			margin-left: 11px;
-		}
-		> span:first-of-type {
-			margin-left: 0;
-		}
-	}
-	.rating_count {
-		font-size: 12px;
-		margin-left: ${props => (props.simple ? '2px' : '0px')};
-		letter-spacing: 0px;
-	}
+  display: flex;
+  flex-direction: ${props => (props.simple ? "row" : "column")};
+  align-items: center;
+  width: 100%;
+  .rating_area {
+    margin-top: -2px;
+  }
+
+  .rating_count {
+    font-size: 12px;
+    margin-left: ${props => (props.simple ? "2px" : "0px")};
+    letter-spacing: 0px;
+  }
 `;
 
 export default RatingStarStyled;
