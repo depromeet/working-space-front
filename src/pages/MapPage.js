f2619d9b2978c7b712f5ea00010525ddb8a85a99
simsimjae <simsimjae@naver.com>
2020-08-17 13:39:07 +0900

    flex 컨테이너로 변경

--- src/pages/MapPage.js
+++ src/pages/MapPage.js
@@ -4,10 +4,10 @@ import HeaderContainer from "../containers/HeaderContainer";
 
 const MapPage = () => {
   return (
-    <>
+    <div style={{ display: "flex", flexDirection: "column", height: "100%" }}>
       <HeaderContainer hasBackButton={true} hasShareButton={false} hasMapButton={false} hasLocalText={true} hasLocationButton={false} />
-      <MapContainer />
-    </>
+      <MapContainer style={{ flex: 1 }} />
+    </div>
   );
 };
 
