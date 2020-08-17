09645e2805abfdc739a2b974a1419f44c956e3d8
leejiwonn <bbongwa@naver.com>
2020-08-17 23:09:57 +0900

    tag store 연결

--- src/components/Detail/Detail.js
+++ src/components/Detail/Detail.js
@@ -7,11 +7,11 @@ import DetailLocation from "../DetailInfo/DetailLocation";
 import TagList from "../Tag/TagList";
 
 const Detail = props => {
-  const { card, tagCount, hasMainShow, mapRef } = props;
+  const { card, hasMainShow, mapRef } = props;
 
   return (
     <DetailStyled>
-      <DetailTitle title={card.title} distance={card.distance} tagCount={tagCount} />
+      <DetailTitle title={card.title} distance={card.distance} tagCount={card.tags.length} />
       <Swiper />
       <DetailInfo />
       <hr />
@@ -19,7 +19,7 @@ const Detail = props => {
         <div className="tag-title">
           <h2 className="tag-text">태그</h2>
         </div>
-        <TagList hasMainShow={hasMainShow} hasMoreTags={false} hasDropDownButton={true} isShowFollow={true} />
+        <TagList tags={card.tags} hasMainShow={hasMainShow} hasMoreTags={false} hasDropDownButton={true} isShowFollow={true} />
       </div>
       <hr />
       <DetailLocation mapRef={mapRef} />
@@ -29,7 +29,24 @@ const Detail = props => {
 
 Detail.defaultProps = {
   tagCount: 5,
-  card: { title: "Cafe1", distance: "2.2km" },
+  card: {
+    title: "Cafe1",
+    distance: "2.2km",
+    tags: [
+      { name: "study", follow: 12, isSelected: false },
+      { name: "concent", follow: 23, isSelected: false },
+      { name: "mute", follow: 21, isSelected: false },
+      { name: "wifi", follow: 16, isSelected: false },
+      { name: "parking", follow: 7, isSelected: false },
+      { name: "dessert", follow: 2, isSelected: false },
+      { name: "toilet", follow: 3, isSelected: false },
+      { name: "twentyFour", follow: 4, isSelected: false },
+      { name: "smoking", follow: 5, isSelected: false },
+      { name: "timer", follow: 6, isSelected: false },
+      { name: "seat", follow: 7, isSelected: false },
+      { name: "chair", follow: 1, isSelected: false },
+    ],
+  },
   mapRef: null,
 };
 
--- src/components/Tag/Tag.js
+++ src/components/Tag/Tag.js
@@ -1,32 +1,6 @@
 import React, { useCallback } from "react";
 import TagStyled from "./Tag.styles";
-import { ReactComponent as ConcentIcon } from "../../images/icon-concent.svg";
-import { ReactComponent as MuteIcon } from "../../images/icon-mute.svg";
-import { ReactComponent as ParkingIcon } from "../../images/icon-parking.svg";
-import { ReactComponent as WifiIcon } from "../../images/icon-wifi.svg";
-import { ReactComponent as ToiletIcon } from "../../images/icon-toilet.svg";
-import { ReactComponent as ChairIcon } from "../../images/icon-chair.svg";
-import { ReactComponent as TwentyFourIcon } from "../../images/icon-24-h.svg";
-import { ReactComponent as TimerIcon } from "../../images/icon-timer.svg";
-import { ReactComponent as StudyIcon } from "../../images/icon-study.svg";
-import { ReactComponent as DessertIcon } from "../../images/icon-dessert.svg";
-import { ReactComponent as SmokingIcon } from "../../images/icon-smoking.svg";
-import { ReactComponent as SeatIcon } from "../../images/icon-seat.svg";
-
-const tagIcons = {
-  concent: <ConcentIcon />,
-  mute: <MuteIcon />,
-  parking: <ParkingIcon />,
-  toilet: <ToiletIcon />,
-  twentyFour: <TwentyFourIcon />,
-  study: <StudyIcon />,
-  dessert: <DessertIcon />,
-  smoking: <SmokingIcon />,
-  timer: <TimerIcon />,
-  seat: <SeatIcon />,
-  wifi: <WifiIcon />,
-  chair: <ChairIcon />,
-};
+import { tagNameByType, tagImageByType } from "../../constants/tagType";
 
 const Tag = ({ tag, isSelectable, isSelected, isShowFollow, onClick }) => {
   const handleClick = useCallback(() => {
@@ -36,8 +10,10 @@ const Tag = ({ tag, isSelectable, isSelected, isShowFollow, onClick }) => {
   return (
     <TagStyled isSelectable={isSelectable} isSelected={isSelected}>
       <div className="tag" onClick={handleClick}>
-        <span className="tag_icon">{tagIcons[tag.iconName]}</span>
-        <span className="tag_text">{tag.text}</span>
+        <span className="tag_icon">
+          <img src={tagImageByType[tag.name]} alt="" />
+        </span>
+        <span className="tag_text">{tagNameByType[tag.name]}</span>
         {isShowFollow && tag.follow > 0 && <span className="tag_follow">+{tag.follow}</span>}
       </div>
     </TagStyled>
@@ -45,7 +21,7 @@ const Tag = ({ tag, isSelectable, isSelected, isShowFollow, onClick }) => {
 };
 
 Tag.defaultProps = {
-  tag: { iconName: "concent", text: "콘센트가 많아요", follow: 11 },
+  tag: { name: "concent", follow: 11 },
   isShowFollow: true,
   isSelectable: false,
   isSelected: false,
