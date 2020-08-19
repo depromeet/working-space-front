132b565b227d15c563f049c595a4ceeb5ac72bca
simsimjae <simsimjae@naver.com>
2020-08-19 20:27:40 +0900

    평가 완료후 CTA버튼 비노출

--- src/components/ModalEvaluation/ModalEvaluation.js
+++ src/components/ModalEvaluation/ModalEvaluation.js
@@ -10,6 +10,7 @@ const ModalEvaluation = props => {
   const { totalStep } = props;
   const [step, setStep] = useState(1);
   const [isActive, setIsActive] = useState(props.isActive);
+  const [isShow, setIsShow] = useState(props.isShow);
   const [mainTitle] = useState(props.mainTitle);
   const [subTitle, setSubTitle] = useState(props.subTitle);
   const [isFooterDisabled, setIsFooterDisabled] = useState(props.isFooterDisabled);
@@ -46,6 +47,7 @@ const ModalEvaluation = props => {
   const onSubmit = useCallback(onModalClose => {
     const selectedTags = tags.filter(tag => tag.isSelected);
     window.localStorage.setItem('working-space', JSON.stringify({ rating, tags: selectedTags.map(tag => tag.name)}))
+    setIsShow(false);
     handleClose(onModalClose);
   }, [rating, tags]);
 
@@ -83,25 +85,29 @@ const ModalEvaluation = props => {
   }, [step, isActive, isFooterDisabled]);
 
   return (
-    <Modal shouldCloseOnDimmedClick={false}>
-      {({ onClickOpen, onClickClose, isOpen, setIsOpen }) => (
-        <styled.ModalContents isActive={isActive}>
-          <div className="header">
-            <h1 className="main_title">{mainTitle}</h1>
-            <h2 className="sub_title">{subTitle}</h2>
-          </div>
-          {step !== 1 && <BackIcon className="back_icon" onClick={handleBackButtonClick} />}
-          <CloseIcon className="close_icon" onClick={() => handleClose(onClickClose)} />
-          {step === 1 && <FirstStep onRatingChange={handleRatingChange} isActive={isActive} />}
-          {step === 2 && <SecondStep onTagsChange={handleTagsChange} tags={tags} onSetTags={setTags} />}
-          <div className="footer">
-            <button type="button" className="footer_button" onClick={() => handleFooterButtonClick(onClickClose)}>
-              {footerButtonText}
-            </button>
-          </div>
-        </styled.ModalContents>
+    <>
+      {isShow && (
+        <Modal shouldCloseOnDimmedClick={false}>
+          {({ onClickOpen, onClickClose, isOpen, setIsOpen }) => (
+            <styled.ModalContents isActive={isActive}>
+              <div className="header">
+                <h1 className="main_title">{mainTitle}</h1>
+                <h2 className="sub_title">{subTitle}</h2>
+              </div>
+              {step !== 1 && <BackIcon className="back_icon" onClick={handleBackButtonClick} />}
+              <CloseIcon className="close_icon" onClick={() => handleClose(onClickClose)} />
+              {step === 1 && <FirstStep onRatingChange={handleRatingChange} isActive={isActive} />}
+              {step === 2 && <SecondStep onTagsChange={handleTagsChange} tags={tags} onSetTags={setTags} />}
+              <div className="footer">
+                <button type="button" className="footer_button" onClick={() => handleFooterButtonClick(onClickClose)}>
+                  {footerButtonText}
+                </button>
+              </div>
+            </styled.ModalContents>
+          )}
+        </Modal>
       )}
-    </Modal>
+    </>
   );
 };
 
@@ -110,6 +116,7 @@ ModalEvaluation.defaultProps = {
   subTitle: "이 카페의 평점은 몇점인가요?",
   totalStep: 2,
   isActive: false,
+  isShow: true,
   footerButtonText: "",
   isFooterDisabled: false,
   rating: 0,
