2efe67f0f268c0dd2e0237ee3f8f7ecad288cda8
leejiwonn <bbongwa@naver.com>
2020-08-19 18:43:21 +0900

    Modal 컴포넌트 tag 사라짐 현상 해결

--- src/models/CardModel.js
+++ src/models/CardModel.js
@@ -12,7 +12,20 @@ class CardModel {
   @observable imageUrl = "https://placehold.it/300x150";
   @observable imageAlt = "카페 이미지";
   @observable rating = 4.5;
-  @observable tagCount = 7;
+  @observable tags = [
+    { name: "study", follow: 12, isSelected: false },
+    { name: "concent", follow: 23, isSelected: false },
+    { name: "mute", follow: 21, isSelected: false },
+    { name: "wifi", follow: 16, isSelected: false },
+    { name: "parking", follow: 7, isSelected: false },
+    { name: "dessert", follow: 2, isSelected: false },
+    { name: "toilet", follow: 3, isSelected: false },
+    { name: "twentyFour", follow: 4, isSelected: false },
+    { name: "smoking", follow: 5, isSelected: false },
+    { name: "timer", follow: 6, isSelected: false },
+    { name: "seat", follow: 7, isSelected: false },
+    { name: "chair", follow: 1, isSelected: false },
+  ];
 
   constructor(data) {
     set(this, {
