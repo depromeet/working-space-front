579dfb427423df3fd1b25339401d172bad21e3b7
simsimjae <191429@ssg.com>
2020-08-21 16:54:52 +0900

    api 변경에 의한 에러 수정

--- src/models/CardModel.js
+++ src/models/CardModel.js
@@ -1,14 +1,14 @@
 import { observable, set } from "mobx";
 
-const imageIndex = 0;
 class CardModel {
   @observable id = "Cafe";
   @observable name = "커피베이 가락점";
   @observable brandName = "커피베이";
-  @observable address = "서울 송파구 오금로34길";
+  @observable address = "서울 송파구 가락동 170-11";
+  @observable roadAddress = "서울 송파구 오금로42길 5";
   @observable startHours = "오전 9시";
   @observable endHours = "오후 11시";
-  @observable location = "서울특별시 용산구 청파로";
+  @observable location = [127.122375827317, 37.4995775321982];
   @observable homepage = null;
   @observable imageUrl = "https://placehold.it/300x150";
   @observable imageAlt = "카페 이미지";
@@ -34,10 +34,11 @@ class CardModel {
       name: data.name,
       brandName: data.brand_name,
       address: data.parcel_addr,
+      roadAddress: data.road_addr,
       startHours: data.start_hours,
       endHours: data.end_hours,
-      location: JSON.parse(data.location),
       homepage: data.homepage,
+      location: data.location.coordinates,
       imageUrl: `/images/${parseInt(Math.random() * 10, 10) + 1}.jpg`,
       imageAlt: `${data.name}이미지`,
     });
