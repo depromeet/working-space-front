bd1e45190978f1e1304725c1390ac8f710e7ddd0
Shim MunSeong <Puterism.K@gmail.com>
2020-08-24 19:19:08 +0900

    불필요한 코드 삭제

--- src/containers/MapContainer.js
+++ src/containers/MapContainer.js
@@ -196,10 +196,6 @@ const MapContainer = () => {
     setViewportHeight();
   }, [setViewportHeight]);
 
-  useEffect(() => {
-    console.log(cafeData);
-  }, [cafeData]);
-
   return (
     <>
       <Map mapRef={mapRef} isSelected={!!nowSelectingCafe}>
