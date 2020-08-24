867727f448335caa7de90bbef6fd09fbeb62e301
Shim MunSeong <Puterism.K@gmail.com>
2020-08-24 15:44:30 +0900

    PC, 모바일 접속 구분하여 카카오맵 링크 다르게 주도록 구현

--- src/components/DetailInfo/DetailLocation.js
+++ src/components/DetailInfo/DetailLocation.js
@@ -1,14 +1,15 @@
 import React from "react";
 import DetailLocationStyled from "./DetailLocation.styles";
+import isMobile from "../../utils/IsMobileUtils";
 
 const DetailLocation = props => {
-  const { mapRef, latitude, longitude } = props;
+  const { mapRef, latitude, longitude, dataId } = props;
 
   return (
     <DetailLocationStyled>
       <h2 className="location-title">위치</h2>
 
-      <a href={`kakaomap://route?ep=${latitude},${longitude}&by=FOOT`}>
+      <a href={isMobile() ? `kakaomap://route?ep=${latitude},${longitude}&by=FOOT` : `https://map.kakao.com/link/to/${dataId}`}>
         <div className="location-map" ref={mapRef}></div>
       </a>
     </DetailLocationStyled>
@@ -19,6 +20,7 @@ DetailLocation.defaultProps = {
   mapRef: null,
   latitude: null,
   longitude: null,
+  dataId: null,
 };
 
 export default DetailLocation;
